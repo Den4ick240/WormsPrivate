f6a65430be3810e4b7774ad6476f85f8b1389574
Den4ick240 <zhigalovden@mail.ru>
2021-12-22 11:56:55 +0700

    added equals method to Position class

--- Worms.Domain/Position.cs
+++ Worms.Domain/Position.cs
@@ -16,5 +16,23 @@ namespace Worms.Domain
         {
             return Math.Abs(X - position.X) + Math.Abs(Y - position.Y);
         }
+
+        protected bool Equals(Position other)
+        {
+            return X == other.X && Y == other.Y;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Position) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(X, Y);
+        }
     }
 }
